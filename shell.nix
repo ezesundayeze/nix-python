6915f9212c0cc71edf01d8e76c08ee9018ef90c4
Eze Sunday <mailsforeze@gmail.com>
2023-04-09 04:17:22 +0100

    refactor

--- shell.nix
+++ shell.nix
@@ -5,11 +5,10 @@ let
 in
 
 pkgs.mkShell {
-  name = "myenv";
   buildInputs = [ python ];
   shellHook = ''
     python -m venv venv
-    source env/bin/activate
+    source venv/bin/activate
     pip install -r requirements.txt
     python -m unittest unit_test.py
   '';
